9e203e747bc3cf5cf0dbba2593b6ab0d4284abc1
Stephen Crane <scrane88@gmail.com>
2021-07-18 15:19:18 -0400

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const listOfMovies = movies.map(movie => {
+    return <div key={movie.title}>
+      <h3>{movie.title}</h3>
+      <p>Runtime: {movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {movie.genres.map(genre => <li key={Math.random() * 5000}>{genre}</li>)}
+      </ul>
+    </div>
+  })
+  
+  return <div>
+    <h1>Movies Page</h1>
+      {listOfMovies}
+    </div>;
 }
 
 export default Movies;
